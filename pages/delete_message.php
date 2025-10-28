f97ec133ca2d3a6ed79e9f55a6ae7b3f74ee695b
SystemDeveloper11 <remotaskfreelancer@gmail.com>
2025-10-28 11:16:47 +0300

    Update delete_message.php

--- pages/delete_message.php
+++ pages/delete_message.php
@@ -3,7 +3,7 @@
 session_start();
 require_once __DIR__ . '/../config/config.php';
 
-// Check if user is logged in
+// Check if user is logged in 
 if (!isset($_SESSION['user_id'])) {
     header('HTTP/1.1 401 Unauthorized');
     echo json_encode(['success' => false, 'error' => 'Not logged in']);
